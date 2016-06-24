ac14cd77a02344eff700fc489d5ec28c713894c1
Your Name <shubhamgupta190496@gmail.com>
2016-06-24 18:19:31 +0530

    CRUD applied to views

--- django_project/posts/views.py
+++ django_project/posts/views.py
@@ -3,5 +3,17 @@ from django.http import HttpResponse
 
 # Create your functional views here.
 
-def post_home(request):
-	return HttpResponse("<h1> Django </h1>")
+def post_create(request):
+	return HttpResponse("<h1> Create </h1>")
+
+def post_detail(request):
+	return HttpResponse("<h1> Detail </h1>")
+
+def post_list(request):
+	return HttpResponse("<h1> List </h1>")
+
+def post_update(request):
+	return HttpResponse("<h1> Update </h1>")
+
+def post_delete(request):
+	return HttpResponse("<h1> Delete </h1>")
