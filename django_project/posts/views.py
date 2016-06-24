0ccde34d6923aae86e4dc75d7e85d352cdef95d8
Your Name <shubhamgupta190496@gmail.com>
2016-06-24 17:34:24 +0530

    Added my first view

--- django_project/posts/views.py
+++ django_project/posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your functional views here.
+
+def post_home(request):
+	return HttpResponse("<h1> Django </h1>")
