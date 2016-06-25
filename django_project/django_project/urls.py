a46067693ed4926495b73a36bfb81cc8c67215b4
Your Name <shubhamgupta190496@gmail.com>
2016-06-25 17:40:38 +0530

    changes made for including static files

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.conf.urls import include,url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from posts import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include('posts.urls',namespace="posts")),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
