2a1eb50168401595ae63b2ce43193700bd8dc624
Your Name <shubhamgupta190496@gmail.com>
2016-06-25 13:59:16 +0530

    query sets and templates added

--- django_project/posts/views.py
+++ django_project/posts/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your functional views here.
 
 def post_create(request):
 	return HttpResponse("<h1> Create </h1>")
 
 def post_detail(request):
-	return HttpResponse("<h1> Detail </h1>")
+	context = {
+	"title" : "detail"
+	}
+	return render(request,"index.html",context)
 
 def post_list(request):
-	return HttpResponse("<h1> List </h1>")
+	obj=Post.objects.all() 
+	context = {
+	"object_list" : obj , 
+	"title" : "list"
+	}
+	# if request.user.is_authenticated(): # will give weather the user is authenticated or not
+	# 	context = {
+	# 	"title" : "My list"
+	# 	}
+	# else:
+	# 	context = {
+	# 	"title" : "list"
+	# 	}	
+	return render(request,"index.html",context)
+	# return HttpResponse("<h1> List </h1>")
 
 def post_update(request):
 	return HttpResponse("<h1> Update </h1>")
