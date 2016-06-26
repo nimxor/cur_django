b7e7c2bbc55308e10f21ffce9cece577fdc80bf8
Your Name <shubhamgupta190496@gmail.com>
2016-06-26 17:01:58 +0530

    imagefield added to models

--- django_project/posts/views.py
+++ django_project/posts/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render , get_object_or_404 ,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import messages
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 from .models import Post
 from .forms import PostForm
 
 # Create your functional views here.
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		print form.cleaned_data.get('title')
@@ -37,9 +38,20 @@ def post_detail(request,id=None):
 	return render(request,"post_detail.html",context)
 
 def post_list(request):
-	obj=Post.objects.all() 
+	queryset_list=Post.objects.all()
+	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+	# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+	# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
 	context = {
-	"object_list" : obj , 
+	"object_list" : queryset , 
 	"title" : "list"
 	}
 	# if request.user.is_authenticated(): # will give weather the user is authenticated or not
@@ -53,9 +65,10 @@ def post_list(request):
 	return render(request,"post_list.html",context)
 	# return HttpResponse("<h1> List </h1>")
 
+
 def post_update(request,id=None):
 	instance = get_object_or_404(Post,id=id) # just a type of a query set
-	form = PostForm(request.POST or None,instance = instance)
+	form = PostForm(request.POST or None,request.FILES or None,instance = instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		print form.cleaned_data.get('title')
