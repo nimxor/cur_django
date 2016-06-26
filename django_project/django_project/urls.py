bfd84ebb0eafaf72dbad455a66fc8ce5606e3059
Your Name <shubhamgupta190496@gmail.com>
2016-06-26 16:02:26 +0530

    added some bootstrap

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from posts import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -28,3 +27,4 @@ urlpatterns = [
 
 if settings.DEBUG:
 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
